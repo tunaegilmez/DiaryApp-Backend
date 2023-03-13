4fd3878793ac70063a5d18849e467734e0456526
tunaegilmez <tunaegilmez07@gmail.com>
2023-03-13 22:21:04 +0300

    user's input data validate

--- modules/auth/controller.js
+++ modules/auth/controller.js
@@ -1,8 +1,19 @@
 import Service from "./service.js";
+import Joi from "@hapi/joi";
+
+const registerSchema = Joi.object({
+  name: Joi.string().required().min(3).max(255),
+  email: Joi.string().required().email().min(6).max(255),
+  password: Joi.string().required().min(6).max(255),
+});
 
 const addUser = async (req, res) => {
   const { name, email, password } = req.body;
-
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
   try {
     let addedUser = await Service.addUser(name, email, password);
     return res.json({ status: true, addedUser });
--- modules/auth/model.js
+++ modules/auth/model.js
@@ -4,23 +4,10 @@ const User = mongoose.model(
   "User",
   new mongoose.Schema(
     {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      },
+      name: { type: String, required: true, min: 3, max: 255 },
+      email: { type: String, required: true, min: 6, max: 255, unique: true },
+      password: { type: String, required: true, min: 6, max: 1024 },
+      date: { type: Date, default: Date.now },
     },
     { timestamps: true }
   )
