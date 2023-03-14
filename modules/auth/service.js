c96afcc9d484167a5465e93c209ec263c707de55
tunaegilmez <tunaegilmez07@gmail.com>
2023-03-14 20:00:53 +0300

    password hashed

--- modules/auth/service.js
+++ modules/auth/service.js
@@ -1,6 +1,10 @@
 import Model from "./model.js";
+import { hashPassword } from "../utilities/bcrypt.js";
 
 const addUser = async (name, email, password) => {
+  if (password) {
+    password = await hashPassword(password);
+  }
   return new Model.User({ name, email, password }).save();
 };
 
